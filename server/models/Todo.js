ac5f3d8a47944a31da2e4bb05aefe11d512f4bf0
NuclMe <saltansergei@gmail.com>
2024-10-09 01:35:56 +0300

    added backend

---
+++ server/models/Todo.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  status: { type: String, required: true, default: 'Todo' },
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
