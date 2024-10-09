128fa0f495c4abe4e1c454701e90f6bd2bbe95ff
NuclMe <saltansergei@gmail.com>
2024-10-09 23:32:33 +0300

    added routes for board and moved frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Header, Columns } from './components';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Columns />
+    </>
+  );
+}
+
+export default App;
